d295a17ba7a5b038109ac882237fd4f92014fe68
CubiZm <karelense@gmail.com>
2017-05-25 14:27:48 +0300

    module1-task2 | добавляет версию рабочего кода

---
+++ js/main.js
@@ -0,0 +1,35 @@
+const LEFT_ARROW = 37;
+const RIGHT_ARROW = 39;
+
+const app = document.querySelector(`.app`);
+const template = document.querySelector(`#templates`).content;
+const arrTemplates = [...template.querySelectorAll(`.main`)];
+
+const startScreen = arrTemplates.findIndex((screen) => screen.classList.contains(`main--welcome`));
+
+let currentIndex = startScreen;
+
+const showScreen = () => {
+  const stepContent = arrTemplates[currentIndex];
+  app.innerHTML = ``;
+  app.appendChild(stepContent);
+};
+
+showScreen(startScreen);
+
+const changeIndex = (evt, index) => {
+  if (evt.altKey) {
+    switch (evt.keyCode) {
+      case RIGHT_ARROW:
+        currentIndex = index === arrTemplates.length - 1 ? 0 : index + 1;
+        break;
+      case LEFT_ARROW:
+        currentIndex = index ? index - 1 : arrTemplates.length - 1;
+    }
+    showScreen(currentIndex);
+  }
+};
+
+document.addEventListener(`keydown`, function (evt) {
+  changeIndex(evt, currentIndex);
+});
