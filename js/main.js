b3a610c60fd5537154fa95d9763ed552730af29a
CubiZm <seo-xcube@yandex.ru>
2017-05-26 14:47:54 +0300

    module1-task2 | Правит вторую часть замечаний

--- js/main.js
+++ js/main.js
@@ -1,39 +1,46 @@
-const KeyCode = {
-  LEFT: 37,
-  RIGHT: 39
-};
+(function () {
+  const KeyCode = {
+    LEFT: 37,
+    RIGHT: 39
+  };
 
-const app = document.querySelector(`.app`);
-const template = document.querySelector(`#templates`).content;
-const arrTemplates = [...template.querySelectorAll(`.main`)];
-
-const startScreen = `main--welcome`;
-const screenIndex = arrTemplates.findIndex((screen) => screen.classList.contains(startScreen));
-
-let currentIndex = screenIndex;
-
-const showScreen = () => {
-  const stepContent = arrTemplates[currentIndex];
-  app.innerHTML = ``;
-  app.appendChild(stepContent);
-};
-
-const changeIndex = (evt, index) => {
+  const appElement = document.querySelector(`.app`);
+  const template = document.querySelector(`#templates`).content;
+  const arrTemplates = [...template.querySelectorAll(`.main`)];
+  const startScreen = `main--welcome`;
+  const startScreenIndex = arrTemplates.findIndex((screen) => screen.classList.contains(startScreen));
+  let currentIndex = startScreenIndex;
   let step = arrTemplates.length;
-  if (evt.altKey) {
-    switch (evt.keyCode) {
-      case KeyCode.RIGHT:
-        currentIndex = index === step - 1 ? 0 : index + 1;
-        break;
-      case KeyCode.LEFT:
-        currentIndex = index ? index - 1 : step - 1;
+
+  const showScreen = () => {
+    const stepContent = arrTemplates[currentIndex];
+    appElement.innerHTML = ``;
+    appElement.appendChild(stepContent);
+  };
+
+  const rightSwap = () => {
+    currentIndex = currentIndex === step - 1 ? 0 : currentIndex + 1;
+  };
+
+  const leftSwap = () => {
+    currentIndex = currentIndex ? currentIndex - 1 : step - 1;
+  };
+
+  const changeScreen = (evt) => {
+    if (evt.altKey) {
+      switch (evt.keyCode) {
+        case KeyCode.RIGHT:
+          rightSwap();
+          break;
+        case KeyCode.LEFT:
+          leftSwap();
+      }
+      showScreen();
     }
-    showScreen(currentIndex);
-  }
-};
+  };
 
-showScreen();
+  showScreen();
 
-document.addEventListener(`keydown`, function (evt) {
-  changeIndex(evt, currentIndex);
-});
+  document.addEventListener(`keydown`, changeScreen);
+
+}());
