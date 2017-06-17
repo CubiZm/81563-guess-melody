e4fb4531232e8f4edf9cd88679eee87d69b36c84
CubiZm <seo-xcube@yandex.ru>
2017-06-17 16:06:11 +0300

    module3-task1 | Отлаголивает функцию

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import showScreen from './modules/utils/show-screen';
-import startScreen from './modules/templates/screen/start-screen';
+import showStartScreen from './modules/templates/screen/start-screen';
 import data from './modules/data/statistic';
 
-showScreen(startScreen(data));
+showScreen(showStartScreen(data));
